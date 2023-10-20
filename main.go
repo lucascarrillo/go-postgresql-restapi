36044fd97942fce94519af218713c0052bf0f406
lucas carrillo <lucas.carrillo@gmail.com>
2023-10-20 16:01:28 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", routes.HomeHandler)
+
+	http.ListenAndServe(":3000", r)
+}
